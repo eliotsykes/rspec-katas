f19b57d47aa9a3903f07731f08d03826918e0ecf
Eliot Sykes <eliotsykes@gmail.com>
2014-12-03 21:49:36 +0000

    Snapshotter CLI ready

--- page-snapshot/snapshotter.rb
+++ page-snapshot/snapshotter.rb
@@ -1,5 +1,13 @@
 require_relative "lib/snapshot_taker"
 
+USAGE = "Usage: Provide a URL as the final command line option to see the page's title"
+missing_url = ARGV.length < 1 || ARGV.last == "--help"
+
+if missing_url
+  puts USAGE
+  exit
+end
+
 url = ARGV.last
 title = SnapshotTaker.fetch_title(url)
 puts title
--- page-snapshot/spec/snapshotter_spec.rb
+++ page-snapshot/spec/snapshotter_spec.rb
@@ -1,22 +1,36 @@
 describe "snapshotter.rb" do
 
-  xcontext "called with --help" do
+  EXPECTED_USAGE = "Usage: Provide a URL as the final command line option to see the page's title\n"
+
+  context "called with --help" do
+
     it "prints help" do
+      expect(run_script("--help")).to eq(EXPECTED_USAGE)
     end
+
   end
 
-  xcontext "called without options" do
+  context "called without options" do
+    
     it "prints help" do
+      expect(run_script).to eq(EXPECTED_USAGE)
     end
+  
   end
 
   context "called with a valid URL" do
 
     it "outputs page title on its own line" do
-      output = `ruby page-snapshot/snapshotter.rb "http://en.wikipedia.org/"`
+      output = run_script("http://en.wikipedia.org/")
       expect(output).to eq("Wikipedia, the free encyclopedia\n")
     end
 
+  end
+
+  def run_script(options_str=nil)
+    command = "ruby page-snapshot/snapshotter.rb"
+    command << " #{options_str}" if options_str
+    %x|#{command}|
   end
 
 end
