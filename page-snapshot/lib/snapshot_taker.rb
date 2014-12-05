afed51ab25e57493c3304d67da345a1cac135ed5
Eliot Sykes <eliotsykes@gmail.com>
2014-12-05 10:07:18 +0000

    Spec to test that favicon downloaded as expected

--- page-snapshot/lib/snapshot_taker.rb
+++ page-snapshot/lib/snapshot_taker.rb
@@ -9,16 +9,23 @@ module SnapshotTaker
   end
 
   def self.fetch_favicon(url)
-    favicon_uri = guess_favicon_url
+    favicon_url = guess_favicon_url(url)
+    target_file_path = "downloaded-favicon.ico"
     
+    open(favicon_url) do |source_file|
+      write_binary_file_mode = "wb"
+      
+      File.open(target_file_path, write_binary_file_mode) do |target_file|
+        target_file.puts source_file.read
+      end
+    end
+
+    target_file_path
   end
 
   def self.guess_favicon_url(url)
     uri = URI.parse(url)
-    favicon_uri = URI::HTTP.build({
-      host: uri.host,
-      path: "/favicon.ico"
-    })
+    favicon_uri = URI::HTTP.build({ host: uri.host, path: "/favicon.ico" })
     favicon_uri.to_s
   end
 
--- page-snapshot/spec/snapshot_taker_spec.rb
+++ page-snapshot/spec/snapshot_taker_spec.rb
@@ -13,12 +13,33 @@ describe SnapshotTaker do
 
   context ".guess_favicon_url(url)" do
 
-    it "should build the correct url" do
+    it "should build the correct URL" do
       url = "http://en.wikipedia.org/wiki/Main_Page"
       expect(SnapshotTaker.guess_favicon_url(url)).to eq("http://en.wikipedia.org/favicon.ico")
     end
 
   end
 
+  context ".fetch_favicon(url)" do
+
+    it "should fetch the favicon for the given URL" do
+      
+      url = "http://en.wikipedia.org/wiki/Main_Page"
+      path_for_expected_favicon = "#{Dir.pwd}/page-snapshot/spec/support/favicon-wikipedia.ico"
+      
+      path_for_downloaded_favicon = SnapshotTaker.fetch_favicon(url)
+      
+      expect(md5_hash(path_for_downloaded_favicon)).to(
+        eq(md5_hash(path_for_expected_favicon)),
+        "Downloaded favicon is not as expected"
+      )
+
+    end
+
+  end
+
+  def md5_hash(file_path)
+    Digest::MD5.hexdigest(File.read(file_path))
+  end
 
 end
