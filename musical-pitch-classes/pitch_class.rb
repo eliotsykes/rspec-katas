e470ef7ee54a2f172cf5b2b893ab5d778a1708da
Eliot Sykes <eliotsykes@gmail.com>
2015-09-28 19:28:26 +0100

    Handle sharps and flats

--- musical-pitch-classes/pitch_class.rb
+++ musical-pitch-classes/pitch_class.rb
@@ -9,6 +9,22 @@ NATURAL_NOTES = {
   'B' => 11
 }.freeze
 
+MIN_LIMIT = NATURAL_NOTES.values.first
+MAX_LIMIT = NATURAL_NOTES.values.last
+
+SHARP_MODIFIER = '#'
+FLAT_MODIFIER = 'b'
+
 def pitch_class(pitch_class)
-  NATURAL_NOTES[pitch_class]
+  note = pitch_class[0]
+  modifier = pitch_class[1]
+  result = NATURAL_NOTES[note]
+  if SHARP_MODIFIER == modifier
+    result += 1
+    result = MIN_LIMIT if result > MAX_LIMIT
+  elsif FLAT_MODIFIER == modifier
+    result -= 1
+    result = MAX_LIMIT if result < MIN_LIMIT
+  end
+  result
 end
--- musical-pitch-classes/pitch_class_spec.rb
+++ musical-pitch-classes/pitch_class_spec.rb
@@ -2,7 +2,7 @@ require_relative 'pitch_class'
 
 describe 'pitch_class(pitch_class)' do
 
-  it 'performs expected conversions on natural notes' do
+  it 'converts natural notes' do
     expect_conversion 'C', 0
     expect_conversion 'D', 2
     expect_conversion 'E', 4
@@ -12,10 +12,32 @@ describe 'pitch_class(pitch_class)' do
     expect_conversion 'B', 11
   end
 
+  it 'converts sharps' do
+    expect_conversion 'C#', 1
+    expect_conversion 'D#', 3
+    expect_conversion 'E#', 5
+    expect_conversion 'F#', 6
+    expect_conversion 'G#', 8
+    expect_conversion 'A#', 10
+    expect_conversion 'B#', 0
+  end
+
+  it 'converts flats' do
+
+    expect_conversion 'Db', 1
+    expect_conversion 'Eb', 3
+    expect_conversion 'Fb', 4
+    expect_conversion 'Gb', 6
+    expect_conversion 'Ab', 8
+    expect_conversion 'Bb', 10
+    expect_conversion 'Cb', 11
+  end
+
   private
 
   def expect_conversion(from, expected)
-    expect(pitch_class(from)).to(eq(expected), "Should have converted '#{from}' to #{expected}")
+    actual = pitch_class(from)
+    expect(actual).to(eq(expected), "Should have converted '#{from}' to #{expected}, got: #{actual.inspect}")
   end
 
 end
