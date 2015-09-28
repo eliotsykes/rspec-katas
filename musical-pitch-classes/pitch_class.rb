56f7fe2839f9693ece2b0b73b026f1fc2a27300d
Eliot Sykes <eliotsykes@gmail.com>
2015-09-28 20:21:38 +0100

    Refactor to use ceiling and modulus

--- musical-pitch-classes/pitch_class.rb
+++ musical-pitch-classes/pitch_class.rb
@@ -1,31 +1,14 @@
 NATURAL_NOTES = {
-  'C' => 0,
-  'D' => 2,
-  'E' => 4,
-  'F' => 5,
-  'G' => 7,
-  'A' => 9,
-  'B' => 11
+  'C' => 0, 'D' => 2, 'E' => 4, 'F' => 5, 'G' => 7, 'A' => 9, 'B' => 11
 }.freeze
-
-MODIFIERS = {
-  '#' => 1,
-  'b' => -1,
-  'B#' => -11,
-  'Cb' => 11
-}
-
+MODIFIERS = { '#' => 1, 'b' => 11 }.freeze
 NOTE_REGEX = /\A[A-G][#b]?\z/
+CEILING = 12
 
 def pitch_class(note)
   return nil unless note =~ NOTE_REGEX
   natural_note = note[0]
-  NATURAL_NOTES[natural_note] + modifier(note)
-end
-
-private
-
-def modifier(note)
   suffix = note[1]
-  MODIFIERS[note] || MODIFIERS[suffix] || 0
+  modifier = MODIFIERS[suffix] || 0
+  (NATURAL_NOTES[natural_note] + modifier) % CEILING
 end
