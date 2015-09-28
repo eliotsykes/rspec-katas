a50ef116e99c947db9408b8ca7750136a0097338
Eliot Sykes <eliotsykes@gmail.com>
2015-09-28 19:36:15 +0100

    Introduce MODIFIERS hash

--- musical-pitch-classes/pitch_class.rb
+++ musical-pitch-classes/pitch_class.rb
@@ -9,22 +9,24 @@ NATURAL_NOTES = {
   'B' => 11
 }.freeze
 
-MIN_LIMIT = NATURAL_NOTES.values.first
-MAX_LIMIT = NATURAL_NOTES.values.last
-
 SHARP_MODIFIER = '#'
 FLAT_MODIFIER = 'b'
 
+MODIFIERS = {
+  SHARP_MODIFIER => 1,
+  FLAT_MODIFIER => -1
+}
+
+MIN_LIMIT = NATURAL_NOTES.values.first
+MAX_LIMIT = NATURAL_NOTES.values.last
+
+
+
 def pitch_class(pitch_class)
   note = pitch_class[0]
-  modifier = pitch_class[1]
-  result = NATURAL_NOTES[note]
-  if SHARP_MODIFIER == modifier
-    result += 1
-    result = MIN_LIMIT if result > MAX_LIMIT
-  elsif FLAT_MODIFIER == modifier
-    result -= 1
-    result = MAX_LIMIT if result < MIN_LIMIT
-  end
+  modifier = MODIFIERS[pitch_class[1]] || 0
+  result = NATURAL_NOTES[note] + modifier
+  result = MIN_LIMIT if result > MAX_LIMIT
+  result = MAX_LIMIT if result < MIN_LIMIT
   result
 end
