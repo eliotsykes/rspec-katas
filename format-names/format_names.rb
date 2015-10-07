bde1f235aefc0852858f3e033c0ad345442a2731
Eliot Sykes <eliotsykes@gmail.com>
2015-10-07 18:39:41 +0100

    Refactor format names exercise

--- format-names/format_names.rb
+++ format-names/format_names.rb
@@ -1,13 +1,5 @@
 def list(nameables)
   return '' if nameables.nil? || nameables.empty?
-  nameables.each_with_index.reduce('') do |memo, (nameable, index)|
-    if index == 0
-      separator = ''
-    elsif index == (nameables.length-1)
-      separator = ' & '
-    else
-      separator = ', '
-    end
-    memo << separator << nameable[:name]
-  end
+  names = nameables.map { |nameable| nameable[:name] }
+  names.one? ? names.first : "#{names[0...-1].join(', ')} & #{names.last}"
 end
