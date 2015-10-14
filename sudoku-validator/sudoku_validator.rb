370ab65fe97632b751fa690d426644d0cda353f5
Eliot Sykes <eliotsykes@gmail.com>
2015-10-14 18:27:37 +0100

    Got blocks built and validating

--- sudoku-validator/sudoku_validator.rb
+++ sudoku-validator/sudoku_validator.rb
@@ -8,6 +8,7 @@ module Sudoku
   class Board
 
     VALID_CHUNK = (1..9).to_a.freeze
+    BLOCK_SIZE = 3.freeze
 
     attr_reader :grid
 
@@ -17,14 +18,45 @@ module Sudoku
 
     def solved?
       return false unless grid
+
+      rows = grid
+      columns = build_columns(grid)
+      blocks = build_blocks(grid)
+
+      (rows + columns + blocks).all? { |chunk| valid_chunk?(chunk) }
+    end
+
+    private
+
+    def build_blocks(grid)
+      num_blocks = grid.size
+      blocks_per_row = blocks_per_col = grid.size / BLOCK_SIZE
+
+      blocks = (0...num_blocks).map do |block_index|
+        block = []
+        row_offset = (block_index / blocks_per_row) * BLOCK_SIZE
+        col_offset = (block_index % blocks_per_col) * BLOCK_SIZE
+
+        (0...BLOCK_SIZE).each do |row_index|
+          (0...BLOCK_SIZE).each do |col_index|
+            row = row_index+row_offset
+            col = col_index+col_offset
+            block << grid[row][col]
+          end
+        end
+
+        block
+      end
+
+      blocks
+    end
+
+    def build_columns(grid)
       rows = grid
       columns = (0...grid.size).map do |column_index|
         rows.map { |row| row[column_index] }
       end
-      rows.all? { |row| valid_chunk?(row) } && columns.all? { |col| valid_chunk?(col) }
     end
-
-    private
 
     def valid_chunk?(chunk)
       VALID_CHUNK == chunk.sort
